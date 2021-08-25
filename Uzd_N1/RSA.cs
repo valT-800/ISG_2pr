0516e4959818e45b7ab6b186103c710330a95d17
valT-800 <s037700@ad.viko.lt>
2021-08-25 20:15:51 +0300

    Added RSA Encryption

--- Uzd_N1/RSA.cs
+++ Uzd_N1/RSA.cs
@@ -11,6 +11,28 @@ namespace Uzd_N3
 {
     class RSA
     {
-        
+
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+
+
     }
 }
