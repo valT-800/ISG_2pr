2a4c31305d03a88b878509301fc2cfd06d57bf23
valT-800 <s037700@ad.viko.lt>
2021-08-25 20:33:12 +0300

    Writing encryptedText to the file named "encrypted.txt"

--- Uzd_N1/MainForm.cs
+++ Uzd_N1/MainForm.cs
@@ -23,7 +23,8 @@ namespace Uzd_N3
             var plainText = ByteConverter.GetBytes(richTextBox1.Text);
             var encryptedText = rsa.Encryption(plainText, RSA.ExportParameters(false), false);
             richTextBox2.Text = Convert.ToBase64String(encryptedText, Base64FormattingOptions.InsertLineBreaks);
-            
+
+            File.WriteAllText(fileName, Convert.ToBase64String(encryptedText, Base64FormattingOptions.InsertLineBreaks));
         }
 
         private void decryptButton_Click(object sender, EventArgs e)
